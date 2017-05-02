558f533045514f39ee6e0831e01c689a2e367516
Mark Roddy <markroddy@gmail.com>
2017-05-02 16:05:07 -0700

    Switch to using 'prepend' to inject new behavior as the monkey patch was not working due to the private method

--- lib/action_dispatch/routing/route_set/dispatcher.rb
+++ lib/action_dispatch/routing/route_set/dispatcher.rb
@@ -3,15 +3,22 @@
 # controller implementation. In doing so, we can included the name of the
 # controller and action to be executed in all calls to Rails.logger in the
 # context of any of our application controllers.
+# Note the use of `prepend` to deal with the fact that the `dispatch()` function
+# is class private.
+
+module DispatcherExtensions
+  def dispatch(controller, action, env)
+    Rails.logger.tagged({ controller: controller.to_s, action: action.to_s }) do
+      super()
+    end
+  end
+end
+
 module ActionDispatch
   module Routing
     class RouteSet
       class Dispatcher
-        def dispatch(controller, action, env)
-          Rails.logger.tagged({ controller: controller.to_s, action: action.to_s }) do
-            super(controller, action, env)
-          end
-        end
+        prepend DispatcherExtensions
       end
     end
   end
